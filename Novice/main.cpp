6430e05363a1bc5f948b85b45ef22af7b2e064b2
Naie Htoo Aung <k025g1013@.g.neec.ac.jp>
2026-05-19 12:23:24 +0900

    first commit

--- Novice/main.cpp
+++ Novice/main.cpp
@@ -1,7 +1,78 @@
 #include <Novice.h>
 #include <math.h>
+#include <cstring>
 const char kWindowTitle[] = "GC2A_04_ネイ_トウーアウン";
 
+typedef struct Vector3 {
+
+	float x;
+	float y;
+	float z;
+
+}Vector3;
+
+typedef struct Matrix4x4 {
+
+	float m[4][4];
+
+}Matrix4x4;
+
+Matrix4x4 OrthographicMatrix(float left, float right, float top, float bottom, float nearZ, float farZ) {
+	Matrix4x4 result = {};
+
+	float width = right - left;
+	float height = top - bottom;
+	float depth = farZ - nearZ;
+
+	result.m[0][0] = 2.0f / width;
+	result.m[1][1] = 2.0f / height;
+	result.m[2][2] = 1.0f / depth;
+
+	result.m[3][0] = -(right + left) / width;
+	result.m[3][1] = -(top + bottom) / height;
+	result.m[3][2] = -nearZ / depth;
+
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
+Matrix4x4 PerspectiveMatrix(float fovY, float aspect, float nearZ, float farZ) {
+	Matrix4x4 result = {};
+	float f = 1.0f / tanf(fovY / 2.0f);
+	result.m[0][0] = f / aspect;
+	result.m[1][1] = f;
+	result.m[2][2] = farZ / (farZ - nearZ);
+	result.m[2][3] = 1.0f;
+	result.m[3][2] = -(farZ * nearZ) / (farZ - nearZ);
+	return result;
+}
+Matrix4x4 MakeViewPortMatrix(float x, float y, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 result = {};
+
+	result.m[0][0] = width / 2.0f;
+	result.m[1][1] = -height / 2.0f;
+	result.m[2][2] = maxDepth - minDepth;
+
+	result.m[3][0] = x + width / 2.0f;
+	result.m[3][1] = y + height / 2.0f;
+	result.m[3][2] = minDepth;
+
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+
+			Novice::ScreenPrintf(x + j * kColumnWidth, y + i * kRowHeight+20, "%6.02f", matrix.m[i][j]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -25,7 +96,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		///
 		/// ↓更新処理ここから
 		///
-		
+		Matrix4x4 orthographicMatrix = OrthographicMatrix(-160.f,160.f,200.0f,300.0f,0.0f,1000.0f);
+		Matrix4x4 perspectiveFovMatrix = PerspectiveMatrix(0.63f,1.33f,0.1f,1000.0f);
+		Matrix4x4 viewportMatrix = MakeViewPortMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 		///
 		/// ↑更新処理ここまで
 		///
@@ -33,11 +106,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		///
 		/// ↓描画処理ここから
 		///
-		
+		MatrixScreenPrintf(0, 0, orthographicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
-
+		
 		// フレームの終了
 		Novice::EndFrame();
 
